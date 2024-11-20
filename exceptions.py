6a057df097aa2db5e05cbc3e128610dc06ca61a5
Masha <92044019+Maryymdd@users.noreply.github.com>
2024-11-20 17:46:56 +0300

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -7,3 +7,7 @@ class InvalidGenreException(Exception):
 class InvalidFileTypeException(Exception):
     def __init__(self, file_type):
         super().__init__(f"Неверный тип файла: {file_type}. Допустимые типы: json.")
+
+class BookNotFoundException(Exception):  # Исключение для отсутствия книги в записи о выдаче
+    def __init__(self, book_title):
+        super().__init__(f"Книга '{book_title}' не найдена в записи о выдаче.")
